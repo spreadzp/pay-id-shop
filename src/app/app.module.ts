f460db4af97d13fbd6c1120f310124f0fecbe5f1
pk <pk@theinvaders.pr>
2020-08-01 09:57:35 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, NO_ERRORS_SCHEMA, APP_INITIALIZER } from "@angular/core";
+
+// Firebase Config
+
+import { AppRoutingModule } from "./app-routing.module";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { IndexModule } from "./views/base/index/index.module";
+import { SharedModule } from "./shared/shared.module";
+import { TranslateService } from "./shared/services/translate.service";
+import { ProductModule } from "./views/pages/product/product.module";
+import { UserModule } from "./views/pages/user/user.module";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+
+/* to load and set en.json as the default application language */
+export function setupTranslateFactory(service: TranslateService) {
+  return () => service.use("ru");
+}
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule.withServerTransition({ appId: "serverApp" }),
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    IndexModule,
+    ProductModule,
+    UserModule,
+    SharedModule,
+    ServiceWorkerModule.register("./ngsw-worker.js", {
+      enabled: environment.production,
+      registrationStrategy: "registerImmediately",
+    }),
+    // AngularFireModule.initializeApp(FireBaseConfig),
+    // AngularFireDatabaseModule,
+  ],
+  providers: [
+    TranslateService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: setupTranslateFactory,
+      deps: [TranslateService],
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA],
+})
+export class AppModule {}
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,10 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyCbqAFke0KQQ30US_UyWiVZ3BwwyWbC3oQ",
+  authDomain: "proftech-2003.firebaseapp.com",
+  databaseURL: "https://proftech-2003.firebaseio.com",
+  projectId: "proftech-2003",
+  storageBucket: "proftech-2003.appspot.com",
+  messagingSenderId: "1038095224081",
+  appId: "1:1038095224081:web:488f54f7c23a52f44ad0b0",
+  measurementId: "G-YGK7QKWBCM",
+};
